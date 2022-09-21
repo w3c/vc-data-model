c155816c1b06734cd3a3f8e333dea12b980391b7
Ivan Herman <ivan@ivan-herman.net>
2022-09-21 15:58:10 +0200

    New script for the vocabulary generation

---
+++ vocab/credentials/v2/lib/common.ts
@@ -0,0 +1,89 @@
+/**
+ * Common types and variables.
+ * 
+ * @packageDocumentation
+ */
+
+/**
+ * Placeholder for global data. At the moment, the only thing
+ * it holds is the prefix and the URL of the URL that is being 
+ * handled/
+ */
+export interface Global {
+    vocab_prefix : string;
+    vocab_url    : string;
+}
+
+export const global = {
+    vocab_prefix : "",
+    vocab_url    : ""
+} 
+
+/**
+ * Top level class for a term in general. Pretty much self-explanatory...
+ */
+export interface RDFTerm {
+    id          : string;
+    type        : string[];
+    label       : string;
+    comment     : string;
+    deprecated ?: boolean;
+}
+
+
+/**
+ * Extra information necessary for a class: its superclasses.
+ * None is required.
+ */
+export interface RDFClass extends RDFTerm {
+    subClassOf ?: string[];
+}
+
+/**
+ * Extra information necessary for a property: its superproperties, range, and domain.
+ * None of these are required.
+ */
+export interface RDFProperty extends RDFTerm {
+    subPropertyOf ?: string[];
+    domain        ?: string[];
+    range         ?: string[];
+}
+
+/**
+ * No extra information is necessary for an individual, but it makes the code
+ * more readable if there is a separate interface for it.
+ */
+export interface RDFIndividual extends RDFTerm {
+}
+
+/**
+ * Information for a prefix (to be used either as a prefix in Turtle or in the context of a JSON-LD).
+ */
+export interface RDFPrefix {
+    prefix : string;
+    url    : string;
+}
+
+/**
+ * Information for the ontology properties, i.e., properties that are defined on the top level. 
+ * 
+ * The third value (url) indicates whether the property is a URL value. For values extracted from the CSV
+ * this is decided by checking whether the string can be considered to be a valid URL or not.
+ */
+export interface OntologyProperty {
+    property : string;
+    value    : string;
+    url      : boolean;
+}
+
+/**
+ * A vocabulary consists of prefixes, top level (ontology) properties, classes, properties and,
+ * possibly, individuals…
+ */
+export interface Vocab {
+    prefixes            : RDFPrefix[],
+    ontology_properties : OntologyProperty[]
+    classes             : RDFClass[],
+    properties          : RDFProperty[],
+    individuals         : RDFIndividual[],
+}
---
+++ vocab/credentials/v2/lib/jsonld.ts
@@ -0,0 +1,201 @@
+/**
+ * Convert the internal representation of the vocabulary into JSON-LD 
+ * (see the 'Vocab' interface).
+ * 
+ * @packageDocumentation
+ */
+
+import { Vocab, global } from './common';
+import { promises as fs } from 'fs';
+
+// Generic context. All items may not be used in a specific vocabulary, but it
+// is not harmful to have them here.
+const generic_context = {
+    "dc:title":                 { "@container": "@language" },
+    "dc:description":           { "@container": "@language" },
+    "dc:date":                  { "@type": "xsd:date" },
+    "rdfs:comment":             { "@container": "@language" },
+    "rdfs:domain":              { "@type": "@id" },
+    "rdfs:label":               { "@container": "@language" },
+    "rdfs:range":               { "@type": "@id" },
+    "rdfs:seeAlso":             { "@type": "@id" },
+    "rdfs:subClassOf":          { "@type": "@id" },
+    "rdfs:subPropertyOf":       { "@type": "@id" },
+    "owl:equivalentClass":      { "@type": "@vocab" },
+    "owl:equivalentProperty":   { "@type": "@vocab" },
+    "owl:oneOf":                { "@container": "@list", "@type": "@vocab" },
+    "owl:deprecated":           { "@type": "xsd:boolean" },
+    "owl:imports":              { "@type": "@id" },
+    "owl:versionInfo":          { "@type": "@id" },
+    "owl:inverseOf":            { "@type": "@vocab" },
+    "owl:unionOf":              { "@type": "@vocab", "@container": "@list" },
+    "rdfs_classes":             { "@reverse": "rdfs:isDefinedBy", "@type": "@id" },
+    "rdfs_properties":          { "@reverse": "rdfs:isDefinedBy", "@type": "@id" },
+    "rdfs_instances":           { "@reverse": "rdfs:isDefinedBy", "@type": "@id" },
+}
+
+/**
+ * Generate the JSON-LD representation of the vocabulary.
+ * 
+ * The function does not generate JSON-LD directly; instead, a standard JS object
+ * is generated and the built-in JSON serializer takes care of the idiosyncrasies of 
+ * the JSON syntax.
+ * 
+ * There is one neat trick in the generated code: the JSON-LD code, usually, 
+ * becomes very complicated if the structure is not a tree. This problem is handled here 
+ * by the repeated usage of reverse properties that put, in the JSON sense, 
+ * the vocabulary itself on the top, linked to the individual terms via a 
+ * (reversed) rdfs:isDefinedBy. (See the definition of the 'rdfs_classes', 
+ * 'rdfs_properties', and 'rdfs_instances'). Thanks to Gregg Kellogg for that trick...
+ * 
+ * 
+ * @param fname File name for the generated JSON-LD file
+ * @param vocab The internal representation of the vocabulary
+ * @returns
+ * @async 
+ */
+export function to_jsonld(fname: string, vocab: Vocab): Promise<void> {
+    // Handling of the domain is a bit complicated due to the usage
+    // of the owl:unionOf construct; factored it here to make the 
+    // code more readable.
+    const multi_domain = (value: string[]): any => {
+        if (value.length === 1) {
+            return value[0];
+        } else {
+            return {
+                "owl:unionOf": value
+            };
+        }
+    }
+
+    // This is just for symmetry v.a.v. the domain...
+    const multi_range = (value: string[]): any => {
+        if (value.length === 1) {
+            return value[0];
+        } else {
+            return value;
+        }
+    }
+ 
+    // This is the target object
+    const jsonld: any = {}
+
+    // Creation of the context: take the prefixes from the vocabulary definition
+    // and add the generic context
+    {
+        let context: any = {};
+        for (const prefix of vocab.prefixes) {
+            context[prefix.prefix] = prefix.url
+        }
+        context = {...context, ...generic_context};
+        jsonld["@context"] = context;
+    }
+
+    // That is the core: the ID of the ontology itself!
+    jsonld["@id"] = global.vocab_url;
+    
+    // Here we go, category by category...
+    {
+        // The Ontology properties are all top level...
+        jsonld["@type"] = "owl:Ontology";
+        for (const ont of vocab.ontology_properties) {
+            if (ont.property === 'dc:date' || ont.url) {
+                jsonld[ont.property] = ont.value;
+            } else {
+                jsonld[ont.property] = {
+                    en : ont.value
+                }
+            }
+        }
+    }
+
+    {
+        // Get the classes
+        const classes: any[] = [];
+        for (const cl of vocab.classes) {
+            const cl_object: any = {};
+            cl_object["@id"]   = `${global.vocab_prefix}:${cl.id}`;
+            if (cl.type.length === 1) {
+                cl_object["@type"] = cl.type[0]
+            } else {
+                cl_object["@type"] = cl.type;
+            }
+            if (cl.deprecated) {
+                cl_object["owl:deprecated"] = true
+            }
+            if (cl.subClassOf) {
+                cl_object["rdfs:subClassOf"] = cl.subClassOf;
+            }
+            cl_object["rdfs:label"] = {
+                "en" : cl.label
+            }
+            cl_object["rdfs:comment"] = {
+                "en" : cl.comment
+            }
+            classes.push(cl_object)
+        }
+        if (classes.length > 0) jsonld.rdfs_classes = classes;
+    }
+
+    {
+
+        // Get the properties
+        const properties: any[] = [];
+        for (const prop of vocab.properties) {
+            const pr_object: any = {}
+            pr_object["@id"]   = `${global.vocab_prefix}:${prop.id}`;
+            if (prop.type.length === 1) {
+                pr_object["@type"] = prop.type[0]
+            } else {
+                pr_object["@type"] = prop.type;
+            }
+            if (prop.deprecated) {
+                pr_object["owl:deprecated"] = true
+            }
+            if (prop.subPropertyOf) {
+                pr_object["rdfs:subPropertyOf"] = prop.subPropertyOf;
+            }
+            if (prop.domain) {
+                pr_object["rdfs:domain"] = multi_domain(prop.domain);
+            }
+            if (prop.range) {
+                pr_object["rdfs:range"] = multi_range(prop.range);
+            }
+            pr_object["rdfs:label"] = {
+                "en" : prop.label
+            }
+            pr_object["rdfs:comment"] = {
+                "en" : prop.comment
+            }
+            properties.push(pr_object);
+        }
+        if (properties.length > 0) jsonld.rdfs_properties = properties;
+    }
+
+    {
+        // Get the individuals
+        const individuals: any[] = [];
+        for (const ind of vocab.individuals) {
+            const ind_object: any = {}
+            ind_object["@id"]   = `${global.vocab_prefix}:${ind.id}`;
+            if (ind.type.length === 1) {
+                ind_object["@type"] = ind.type[0]
+            } else {
+                ind_object["@type"] = ind.type;
+            }
+            if (ind.deprecated) {
+                ind_object["owl:deprecated"] = true
+            }
+            ind_object["rdfs:label"] = {
+                "en" : ind.label
+            }
+            ind_object["rdfs:comment"] = {
+                "en" : ind.comment
+            }
+            individuals.push(ind_object);
+        }
+        if (individuals.length > 0) jsonld.rdfs_individuals = individuals;
+    }
+
+    return fs.writeFile(fname, JSON.stringify(jsonld, null, 4),'utf-8');
+}
