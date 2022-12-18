1e381943b7eaa97faad9cb78849853a2001d1a16
Manu Sporny <msporny@digitalbazaar.com>
2022-12-18 16:26:15 -0500

    Update JSON Schema for validFrom/validUntil.

--- schema/verifiable-presentation/verifiable-presentation-schema-test.js
+++ schema/verifiable-presentation/verifiable-presentation-schema-test.js
@@ -18,7 +18,7 @@ describe('Verifiable Presentation', function () {
     });
 
     // Note: this example fails because the dependent VC is not valid;
-    // it is missing an issuanceDate. Both the VP and VC proofs are missing
+    // it is missing an validFrom. Both the VP and VC proofs are missing
     // created, proofPurpose, and verificationMethod fields. The example has
     // been modified to get the test to pass.
     it('should validate example 25 using JSON Schema 2020-12 ', function () {
